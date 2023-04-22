7b70fec0f445728baeed8be4e2cabda1c2e15b88
AXS05000 <axssxs00@gmail.com>
2023-04-22 12:51:30 -0300

    V - 22-04-2023 - 12:50

--- projeto_vendas/settings.py
+++ projeto_vendas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v)$l+-o!0ddq@df24j*8(bm0=9%m)tv#6s2oyriotn39&18__0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['*']
 
 ALLOWED_HOSTS = ['54.207.82.154']
 
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
