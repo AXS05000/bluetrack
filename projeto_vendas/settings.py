e7854899eccaaa0a4f1e014402fd0c9e6baf5330
AXS05000 <axssxs00@gmail.com>
2023-05-11 22:43:18 -0300

    V - 11-05-2023 - 22:43

--- projeto_vendas/settings.py
+++ projeto_vendas/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-v)$l+-o!0ddq@df24j*8(bm0=9%m)tv#6s2oyriotn39&18__0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*']
 
-ALLOWED_HOSTS = ['54.207.82.154']
+#ALLOWED_HOSTS = ['54.207.82.154']
 
 
 # Application definition
