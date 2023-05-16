7293e9fd59673ad805ae0247a45e00924761eff9
AXS05000 <axssxs00@gmail.com>
2023-05-16 00:07:29 -0300

    V - 16-05-2023 - 00:07

--- usuarios/views.py
+++ usuarios/views.py
@@ -6,8 +6,13 @@ from django.shortcuts import redirect, render
 from django_ratelimit.decorators import ratelimit
 
 
-@ratelimit(key='ip', rate='5/d', block=True)  # block=True will prevent the view from being processed
+@ratelimit(key='ip', rate='5/d')  # block=False by default
 def login_view(request):
+    was_limited = getattr(request, 'limited', False)
+    if was_limited:
+        messages.error(request, 'Você excedeu o limite de tentativas de login. Por favor, tente novamente mais tarde.')
+        return render(request, 'registration/login.html', {'error': messages.get_messages(request)})
+        
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -18,8 +23,6 @@ def login_view(request):
         else:
             messages.error(request, 'Email ou senha incorretos')
     return render(request, 'registration/login.html', {'error': messages.get_messages(request)})
-
-
 
 @login_required(login_url='/login/')
 def index(request):
