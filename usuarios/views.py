754b0f6177bc0b070bcd61ee9ef32e660c26749e
AXS05000 <axssxs00@gmail.com>
2023-05-15 23:09:17 -0300

    V - 15-05-2023 - 23:09

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,23 +3,24 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect, render
+from django_ratelimit.decorators import ratelimit
 
 from .forms import CustomUsuarioCreateForm, LoginForm
 
 
+@ratelimit(key='ip', rate='5/d', method=ratelimit.UNSAFE, block=True)  
 class CustomLoginView(LoginView):
     template_name = 'registration/login.html'
 
-    def login_view(request):
-        if request.method == 'POST':
-            username = request.POST['username']
-            password = request.POST['password']
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('dashboard')
-            else:
-                messages.error(request, 'Email ou senha incorretos')
+    def post(self, request, *args, **kwargs):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Email ou senha incorretos')
         return render(request, 'registration/login.html', {'error': messages.get_messages(request)})
 
 
