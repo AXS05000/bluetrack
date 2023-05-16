c4962512e5432ab4164b79f989dd39e2057fcba5
AXS05000 <axssxs00@gmail.com>
2023-05-16 00:11:45 -0300

    V - 16-05-2023 - 00:11

--- projeto_vendas/settings.py
+++ projeto_vendas/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django_ratelimit.middleware.RatelimitMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- usuarios/views.py
+++ usuarios/views.py
@@ -3,17 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from django_ratelimit.decorators import ratelimit
 
-# LEMBRE QUE VC CONFIGUROU O MIDDLEWARE NO SETTINGS.PY COM 'django_ratelimit.middleware.RatelimitMiddleware' ELE TEM QUE SER O PRIMEIRO PARA DAR CERTO.
 
-@ratelimit(key='ip', rate='5/d')  # block=False by default
 def login_view(request):
-    was_limited = getattr(request, 'limited', False)
-    if was_limited:
-        messages.error(request, 'Você excedeu o limite de tentativas de login. Por favor, tente novamente mais tarde.')
-        return render(request, 'registration/login.html', {'error': messages.get_messages(request)})
-        
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -24,6 +16,8 @@ def login_view(request):
         else:
             messages.error(request, 'Email ou senha incorretos')
     return render(request, 'registration/login.html', {'error': messages.get_messages(request)})
+
+
 
 @login_required(login_url='/login/')
 def index(request):
